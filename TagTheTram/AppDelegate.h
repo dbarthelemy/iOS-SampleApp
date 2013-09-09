b8ab8ff2b0824d5d8d06ee4f9f11799e80552527
Barthelemy David <dbarthelemy@imakeit4u.com>
2013-09-09 22:08:34 +0200

    Initial Commit

---
+++ TagTheTram/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TagTheTram
+//
+//  Created by David Barthelemy on 09/09/13.
+//  Copyright (c) 2013 David Barthelemy, iMakeit4U. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TagTheTram/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TagTheTram
+//
+//  Created by David Barthelemy on 09/09/13.
+//  Copyright (c) 2013 David Barthelemy, iMakeit4U. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
