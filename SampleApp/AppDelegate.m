1d3098f6ba42fdf3f204459f6fffdaf3c0abe898
Barthelemy David <dbarthelemy@imakeit4u.com>
2014-01-03 18:19:14 +0100

    Disable Crashlytics.

--- SampleApp/AppDelegate.m
+++ SampleApp/AppDelegate.m
@@ -36,7 +36,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    [Crashlytics startWithAPIKey:@"508e94b3e67f7731eb98d96f4af8ef1e3bf887aa"];
+    [Crashlytics startWithAPIKey:@"YourKeyHere"]; // FIXME
     
     [self handleSettingBundle];
 
